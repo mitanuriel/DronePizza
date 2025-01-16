036a1de3307b70d7b4fb6df99675eb444e1b1a8f
mitanuriel <145558996+mitanuriel@users.noreply.github.com>
2025-01-16 14:49:07 +0100

    Initial setup

---
+++ src/main/java/com/example/dronepizza/DronePizzaApplication.java
@@ -0,0 +1,13 @@
+package com.example.dronepizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronePizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DronePizzaApplication.class, args);
+    }
+
+}
