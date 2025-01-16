c5fa40e2b49ef3deff0543f9f71d67b4f268b227
mitanuriel <145558996+mitanuriel@users.noreply.github.com>
2025-01-16 15:19:56 +0100

    Added redirection from port 8080 to port 8443

--- src/main/java/com/example/dronepizza/config/HttpToHttpsRedirect.java
+++ src/main/java/com/example/dronepizza/config/HttpToHttpsRedirect.java
@@ -1,5 +1,23 @@
 package com.example.dronepizza.config;
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class HttpToHttpsRedirect {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> customizeTomcat (){
+        return factory -> {
+            Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+            connector.setPort(8080);
+            connector.setScheme("http");
+            connector.setSecure(false);
+            connector.setRedirectPort(8443);
+            factory.addAdditionalTomcatConnectors(connector);
+        };
+    }
 
 }
