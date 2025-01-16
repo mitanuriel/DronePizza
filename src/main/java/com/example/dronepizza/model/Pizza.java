a53d2af44c67bcbd7427966d32ff7354dfd752a8
mitanuriel <145558996+mitanuriel@users.noreply.github.com>
2025-01-16 15:22:12 +0100

    Initiated model package, added entity class Pizza

---
+++ src/main/java/com/example/dronepizza/model/Pizza.java
@@ -0,0 +1,44 @@
+package com.example.dronepizza.model;
+import jakarta.persistence.*;
+
+
+@Entity
+public class Pizza {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long pizzaId;
+    private String title;
+    private Integer price;
+
+
+    public Long getPizzaId() {
+        return pizzaId;
+    }
+
+    public void setPizzaId(Long pizzaId) {
+        this.pizzaId = pizzaId;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+
+
+}
